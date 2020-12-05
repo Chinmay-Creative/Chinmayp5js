5950472a51952eefc98a8c08dc19ab7908274e3b
Chinmay-Creative <75534597+Chinmay-Creative@users.noreply.github.com>
2020-12-05 20:32:35 +0530

    Create RevolvingCircle.js

---
+++ RevolvingCircle.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 600);
+}
+
+function draw() {
+  background(220,0,0);
+  print(mouseX,mouseY)
+//circle(400,400,40)
+  
+   
+  ellipse(200,200,60,mouseY/10)
+
+}
