23cd09e62f09475d05f2ef13e76fb584830ec47e
Chinmay-Creative <75534597+Chinmay-Creative@users.noreply.github.com>
2020-12-09 20:58:38 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,24 @@
+//imp notes
+//there are three types of vareables 
+var y=30
+var z=-1.1
+//these are no. vareables
+var a="chinmay"
+var chinmay="13"
+//these are charecters or string vareables
+var k1=true
+var m=y<50
+//these are boolean vareables 
+//vareable name always starts with alphabet
+//and cannot contain spaces,dots or special charecters 
+// We can print these variables to see wath they store
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  print(y)
+  y=50
+  y==50;print(y==50)
+}
