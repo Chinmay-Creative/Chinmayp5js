b875c67350885e9e40dbf0fe4c26b9ef74bd7bda
Chinmay-Creative <75534597+Chinmay-Creative@users.noreply.github.com>
2021-01-30 19:46:31 +0530

    Create Fourballs.js

---
+++ Fourballs.js
@@ -0,0 +1,23 @@
+var a = 300
+var b = 300
+var c = 300
+var d = 300
+var e = 300
+var f = 300
+function setup() {
+  createCanvas(600, 600);
+}
+function draw() {
+  background(220);
+a = a +1
+  b = b -1
+    c = c -1
+    d = d +1
+      e = e -5
+    f = f +5
+  circle(a,300,60)
+    circle(b,b,60)
+    circle(c,d,60)
+      circle(e,f,60)
+
+}
